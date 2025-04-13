6de65ff4d778245bf1d91509dd4a7d4d185097cf
Soumyadip2003-AI <soumyadip.0202@gmail.com>
2025-04-14 05:19:18 +0530

    dfdf

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ from nltk.corpus import stopwords
 import shap
 import torch
 from transformers import BertTokenizer, BertForSequenceClassification
-import cv
+import cv2
 from PIL import Image
 import io
 import requests
